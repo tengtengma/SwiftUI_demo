d4e9cb98c490a3e32c58a656d7b09f5c2717b781
Martin <tengtengma9@gmail.com>
2026-09-17 16:28:54 +1200

    Initial Commit

---
+++ SwiftUI_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Demo
+//
+//  Created by 马腾 on 2026/9/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
